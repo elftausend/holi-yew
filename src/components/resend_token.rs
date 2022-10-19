c267bebcc7df557c465a6e70c850bbbc82b5e26e
elftausend <76885970+elftausend@users.noreply.github.com>
2022-10-19 17:40:24 +0200

    Mount instead of use_effect

--- src/components/resend_token.rs
+++ src/components/resend_token.rs
@@ -1,4 +1,5 @@
 use yew::prelude::*;
+use yew_hooks::use_mount;
 use yew_router::prelude::{use_history, History};
 
 use crate::{
@@ -16,7 +17,7 @@ pub fn auth(props: &Props) -> Html {
     let user_ctx = use_user_context();
     let history = use_history().unwrap();
 
-    use_effect_with_deps(
+    /*use_effect_with_deps(
         move |_| {
             wasm_bindgen_futures::spawn_local(async move {
                 if is_logged_in().await {
@@ -36,7 +37,25 @@ pub fn auth(props: &Props) -> Html {
             || ()
         },
         (), //user_ctx,
-    );
+    );*/
+
+    use_mount(move || {
+        wasm_bindgen_futures::spawn_local(async move {
+            if is_logged_in().await {
+                user_ctx.login(UserInfo {
+                    user_id: user_ctx.inner.user_id.clone(),
+                    token: get_jwt().unwrap()
+                });
+                log::info!("logged in !");
+            } else {
+                log::info!("not logged in !");
+                user_ctx.inner.set(UserInfo {
+                    user_id: user_ctx.inner.user_id.clone(),
+                    token: String::new()
+                })
+            }
+        });
+    });
 
     html! {
         { for props.children.iter() }
